1edbca5c7abad9d54dd005681cf2d71e03a78a68
Victor Hugo Tello <victorhugotello@hotmail.com>
2025-12-19 16:09:57 -0600

    Welcome Page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import WelcomePage from './pages/WelcomePage.vue'
 </script>
 
 <template>
@@ -8,12 +7,12 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>Cotizador</h1>
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <WelcomePage />
   </main>
 </template>
 
