41d3a0a381f94a779ffc8c95b4a3751227e92ed3
Victor Tello <vctrtello@icloud.com>
2026-04-07 18:44:54 -0600

    tiras led

--- src/http/tiras-led-api.js
+++ src/http/tiras-led-api.js
@@ -1,26 +1,26 @@
 import api from "./apl";
 
 export const fetchTirasLed = async () => {
-  const response = await api.get("/tiras-led");
+  const response = await api.get("/tira-leds/");
   return response.data;
 };
 
 export const getTiraLedPorId = async (id) => {
-  const response = await api.get(`/tiras-led/${id}`);
+  const response = await api.get(`/tira-leds/${id}`);
   return response.data;
 };
 
 export const crearTiraLed = async (datos) => {
-  const response = await api.post("/tiras-led", datos);
+  const response = await api.post("/tira-leds/", datos);
   return response.data;
 };
 
 export const actualizarTiraLed = async (id, datos) => {
-  const response = await api.put(`/tiras-led/${id}`, datos);
+  const response = await api.put(`/tira-leds/${id}`, datos);
   return response.data;
 };
 
 export const eliminarTiraLed = async (id) => {
-  const response = await api.delete(`/tiras-led/${id}`);
+  const response = await api.delete(`/tira-leds/${id}`);
   return response.data;
 };
